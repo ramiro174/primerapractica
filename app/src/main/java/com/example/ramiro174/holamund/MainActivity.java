8f26b80acd6ffc23008ac42c13c96d30e42ec68a
Ramiro Esquivel Duran <ramiro174@gmail.com>
2019-01-10 08:09:20 -0600

    agrege un  mensaje de bienvenida

--- app/src/main/java/com/example/ramiro174/holamund/MainActivity.java
+++ app/src/main/java/com/example/ramiro174/holamund/MainActivity.java
@@ -4,19 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Button f= findViewById(R.id.miboton);
-
         f.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Toast.makeText(MainActivity.this, "hola mundo", Toast.LENGTH_SHORT).show();
             }
         });
 
